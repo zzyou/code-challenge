1387fd0c2fb610281fb6d5bd9afbf6b52517f8b8
Zhenzhen You <swimreally@MacBook-Air.local>
2018-03-30 08:57:18 -0700

    vowels and consonants in alternate order function

--- js-function-rithm-classroom6.js
+++ js-function-rithm-classroom6.js
@@ -102,3 +102,30 @@ removeDuplicatesFromSorted([1, 2, 3, 4, 5, 6]); // 6
 removeDuplicatesFromSorted([]); // 0
 
 
+
+/* Create a function isAlt that accepts a string as an argument and validates whether the vowels (a, e, i, o, u) and consonants are in alternate order. */
+
+function isAlt(str) {
+    for (let i = 0; i < str.length - 1; i ++) {
+        if ( (/[aeiou]/.test(str[i]) && /[aeiou]/.test(str[i+1])) || (/[^aeiou]/.test(str[i]) && /[^aeiou]/.test(str[i+1])) ) {
+            return false;
+        }
+    }
+    return true; 
+}
+
+// Another method using .indexOf() === -1:
+function isAlt(word) {
+    var vowels = ['a','e','i','o','u']; 
+    for (var i = 1; i < word.length; i++) {
+      if ((vowels.indexOf(word[i-1]) >= 0 && vowels.indexOf(word[i]) >= 0) || 
+          (vowels.indexOf(word[i-1]) === -1 && vowels.indexOf(word[i]) === -1)) {
+        return false;      
+      }
+    }
+    return true;
+}
+
+isAlt("amazon") // true
+isAlt("apple") // false
+isAlt("banana") // true
