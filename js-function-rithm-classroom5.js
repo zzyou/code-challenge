2763b2a69658ae11d94a96a0d89a827167e31ec3
Zhenzhen <zzyou@users.noreply.github.com>
2018-04-03 19:53:01 -0700

    Update js-function-rithm-classroom5.js

--- js-function-rithm-classroom5.js
+++ js-function-rithm-classroom5.js
@@ -92,10 +92,15 @@ highestScoringWord("this sentence has two highest scoring words"); // "sentence"
 
 
 /* Write a function called beggars that accepts an array and a number. 
-The function should return an array with the sum of what each beggar brings home, assuming they all take regular turns, from the first to the last.
-For example: [1,2,3,4,5] for 2 beggars will return a result of [9,6], as the first one takes [1,3,5], the second collects [2,4].
-The same array with 3 beggars would have in turn have produced a better out come for the second beggar: [5,7,3], as they will respectively take [1,4], [2,5] and [3].
-Not all beggars have to take the same amount of "offers", meaning that the length of the array is not necessarily a multiple of n; length can be even shorter, in which case the last beggars will of course take nothing (0). */
+The function should return an array with the sum of what each beggar brings home, 
+assuming they all take regular turns, from the first to the last.
+For example: [1,2,3,4,5] for 2 beggars will return a result of [9,6], 
+as the first one takes [1,3,5], the second collects [2,4].
+The same array with 3 beggars would have in turn have produced a better out come for the second beggar: [5,7,3], 
+as they will respectively take [1,4], [2,5] and [3].
+Not all beggars have to take the same amount of "offers", 
+meaning that the length of the array is not necessarily a multiple of n; 
+length can be even shorter, in which case the last beggars will of course take nothing (0). */
 
 function beggars(arr, num) {
     if (num === 0) {
